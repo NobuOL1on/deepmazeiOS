951d6c494b478ac44327871d2144cd08d5ed4a1f
Mingyu Lei <leimy@chromium.org>
2025-04-23 02:02:31 +0900

    Pre-refactor change

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   root: path.join(__dirname, 'www'),
   resolve: {
     alias: {
-      'img': path.resolve(__dirname, 'www/img'), // 指向你的 img 目录
+      'img': path.resolve(__dirname, 'www/img'),
     },
   },
 })
